f185681fb2f9451ea0aca4074b447f3ce4ecbd86
Siarhei Yakushevich <yakushevichsv@hotmail.com>
2024-12-01 12:44:20 +0100

    Initial Commit

---
+++ EthFussTests/EthFussTests.swift
@@ -0,0 +1,17 @@
+//
+//  EthFussTests.swift
+//  EthFussTests
+//
+//  Created by Siarhei Yakushevich on 01/12/2024.
+//
+
+import Testing
+@testable import EthFuss
+
+struct EthFussTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
