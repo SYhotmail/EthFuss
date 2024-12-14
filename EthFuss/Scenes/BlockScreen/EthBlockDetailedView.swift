8e49b1ae7ef8faca0762fe7fd03a07a7b0ecc9b7
Siarhei Yakushevich <yakushevichsv@hotmail.com>
2024-12-14 18:41:03 +0100

    detailed block is displayed.

--- EthFuss/Scenes/BlockScreen/EthBlockDetailedView.swift
+++ EthFuss/Scenes/BlockScreen/EthBlockDetailedView.swift
@@ -10,14 +10,55 @@ import SwiftUI
 struct EthBlockDetailedView: View {
     @ObservedObject var viewModel: EthBlockDetailedViewModel
     
+    @ViewBuilder
+    private func rowView(_ info: EthBlockDetailedViewModel.TopSectionRowInfo) -> some View {
+        switch info.type {
+        case .blockHeight(let toolTip, let label, let number, let hasPrevious, let hasNext):
+            
+            HStack {
+                Button(label, systemImage: "questionmark.circle") {
+                    //TODO: show tooltip...
+                }
+                
+                Spacer()
+                
+                Text(number)
+                
+                Image(systemName: "arrowshape.backward")
+                    .padding(5)
+                    .opacity(hasPrevious ? 1 : 0.5)
+                    .onTapGesture(perform: viewModel.loadPrev)
+                
+                Image(systemName: "arrowshape.forward")
+                    .padding(5)
+                    .opacity(hasNext ? 1 : 0.5)
+                    .onTapGesture(perform: viewModel.loadNext)
+                
+            }
+        case .difficulty(let toolTip, let label, let number):
+            HStack {
+                Button(label, systemImage: "questionmark.circle") {
+                    //TODO: show tooltip...
+                }
+                
+                Spacer()
+                
+                Text(number)
+            }
+        }
+    }
+    
     @ViewBuilder var contentView: some View {
         List {
             Section {
-                VStack {
-                    
+                VStack(spacing: 0) {
+                    ForEach(viewModel.topSectionRows) {
+                        rowView($0)
+                    }
                 }
             }
-            //.animation(.default, value: <#T##V#>)
+            .transition(.move(edge: .leading))
+            .animation(.default, value: viewModel.isLoading)
         }.listStyle(.insetGrouped)
     }
     
--- EthFuss/Scenes/BlockScreen/EthBlockDetailedViewModel.swift
+++ EthFuss/Scenes/BlockScreen/EthBlockDetailedViewModel.swift
@@ -7,35 +7,148 @@
 
 import Foundation
 import EthCore
+import Combine
 
 final class EthBlockDetailedViewModel: ObservableObject {
-    private(set)var blockHash: String
-    private(set)var blockNumber: UInt64?
+    private(set)var blockHash: String!
+    private(set)var blockNumber: UInt64!
     private let connector = EthConnector()
     
+    private var task: Task<EthBlockObjectResult, any Error>?
+    private var disposeBag = Set<AnyCancellable>()
+    
     @Published var isLoading = false
-    @Published var ethObject: EthBlockObjectResult?
+    let ethObjectSubject = CurrentValueSubject<EthBlockObjectResult?, Never>(nil)
     @Published var topSectionRows = [TopSectionRowInfo]()
     
-    struct TopSectionRowInfo {
+    struct TopSectionRowInfo: Identifiable {
+        var id: String { type.label }
         let type: TopSectionRowType
-        let data: Any
     }
         
     enum TopSectionRowType {
-        case blockHeight
-        case timestamp
+        case blockHeight(toolTip: String, label: String, number: String, hasPrevious: Bool, hasNext: Bool)
+        case difficulty(toolTip: String, label: String, number: String)
+        
+        var toolTip: String {
+            switch self {
+            case let .blockHeight(toolTip, _, _, _, _):
+                return toolTip
+            case let .difficulty(toolTip, _, _):
+                return toolTip
+            }
+        }
+        
+        var label: String {
+            switch self {
+            case let .blockHeight(_, label, _, _, _):
+                return label
+            case let .difficulty(_, label, _):
+                return label
+            }
+        }
     }
+    
+    private var lastBlockNumber: UInt64?
     
     init(blockHash: String, blockNumber: UInt64?) {
         self.blockHash = blockHash
         self.blockNumber = blockNumber
+        self.lastBlockNumber = blockNumber
+        
+        bind()
+        
+        scheduleBlockLoad(blockNumber: blockNumber, blockHash: blockHash)
     }
     
-    //TODO: load content...
+    private func hasNext() -> Bool {
+        blockNumber != lastBlockNumber // caching value...
+    }
     
+    private func scheduleBlockLoad(positive: Bool) {
+        scheduleBlockLoad(blockNumber: blockNumber.flatMap { positive ? $0 + 1 : $0 - 1 },
+                          blockHash: nil)
+    }
     
+    func loadNext() {
+        guard hasNext() else {
+            return
+        }
+        
+        scheduleBlockLoad(positive: true)
+    }
     
+    func loadPrev() {
+        scheduleBlockLoad(positive: false)
+    }
+    
+    private func bind() {
+        ethObjectSubject.dropFirst()
+            .map { [unowned self] result -> [TopSectionRowInfo] in
+                var info = [TopSectionRowInfo]()
+                if let number = result?.number {
+                    info.append(.init(type: .blockHeight(toolTip: "Also known as a block number",
+                                                         label: "Block Height:",
+                                                         number: number,
+                                                         hasPrevious: true,
+                                                         hasNext: self.hasNext()) ))
+                }
+                
+                if let totalDifficulty = result?.totalDifficulty, let size = result?.size {
+                    info.append(.init(type: .difficulty(toolTip: "Total difficulty of chain",
+                                                        label: "Total Difficulty:",
+                                                        number: /*totalDifficulty*/ size) )) //TODO: check..
+                }
+                
+                return info
+            }
+            .receive(on: DispatchQueue.main)
+            .sink { [unowned self] topSectionRows in
+                self.topSectionRows = topSectionRows
+            }.store(in: &disposeBag)
+    }
+    
+    private func loadBlock(blockNumber: UInt64?, blockHash: String?) async throws -> EthMethodResult<EthBlockObjectResult> {
+        if let blockNumber {
+            return try await self.connector.ethBlockByNumber(tag: .quantity(blockNumber), full: true)
+        } else if let blockHash {
+            return try await self.connector.ethBlockByHash(hash: blockHash, full: true)
+        } else {
+            assertionFailure("Can't be here!")
+            throw NSError(domain: "ethfuss.error", code: .min)
+        }
+    }
+    
+    @MainActor
+    private func setIsLoadingUI(_ isLoading: Bool) {
+        self.isLoading = isLoading
+    }
+    
+    private func scheduleBlockLoad(blockNumber: UInt64?, blockHash: String?) {
+        task?.cancel()
+        task = Task {
+            await self.setIsLoadingUI(true)
+            do {
+                try Task.checkCancellation()
+                let result = try await self.loadBlock(blockNumber: blockNumber,
+                                                      blockHash: blockHash)
+                self.blockNumber = blockNumber
+                self.blockHash = blockHash
+                let value = result.result
+                self.ethObjectSubject.value = value
+                await self.setIsLoadingUI(false)
+                return value
+            } catch {
+                await self.setIsLoadingUI(false)
+                //TODO: key not found therefore last is a prev. one...
+                throw error
+            }
+        }
+    }
+    
+    deinit {
+        disposeBag.forEach { $0.cancel() }
+    }
     
     var title: String? {
         guard let blockNumber else {
